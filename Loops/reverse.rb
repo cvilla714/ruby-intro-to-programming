47f960bff5d746dbf7d4c29ef4b8b39d86424e3e
Cosmel Villalobos <cosmelvg@gmail.com>
2022-06-12 22:06:55 -0400

    Update reverse.rb
    - Include new version of making reverse

--- Loops/reverse.rb
+++ Loops/reverse.rb
@@ -15,3 +15,19 @@ def reverse(word)
   puts reverse("cat")          # => "tac"
   puts reverse("programming")  # => "gnimmargorp"
   puts reverse("bootcamp")     # => "pmactoob"
+
+puts
+puts "new version"
+puts
+  def reverse_next_version(word)
+    new_word = ""
+    word.each_char do |letter|
+        # puts letter
+        new_word = letter + new_word
+    end
+    return new_word
+  end
+
+puts reverse_next_version("cat")          # => "tac"
+puts reverse_next_version("programming")  # => "gnimmargorp"
+puts reverse_next_version("bootcamp")     # => "pmactoob"
