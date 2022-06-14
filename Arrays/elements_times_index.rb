b20b0fc18eae4ab9c6c6ef9c809bd43c37e09825
Cosmel Villalobos <cosmelvg@gmail.com>
2022-06-14 01:44:03 -0400

    Add new file
    - Add new way to iterate each_with_index

---
+++ Arrays/elements_times_index.rb
@@ -0,0 +1,30 @@
+def element_times_index(numbers)
+  newnum = []
+  i = 0
+  while i < numbers.length
+
+    newnum << numbers[i] * i
+
+    i += 1
+  end
+  newnum
+end
+
+puts 'Using simple version'
+print element_times_index([4, 7, 6, 5])       # => [0, 7, 12, 15]
+puts
+print element_times_index([1, 1, 1, 1, 1, 1]) # => [0, 1, 2, 3, 4, 5]
+
+def element_using_each(numbers)
+  newnum = []
+  numbers.each_with_index do |number, idx|
+    newnum << number * idx
+  end
+  newnum
+end
+
+puts
+puts 'Using each with index version'
+print element_using_each([4, 7, 6, 5])       # => [0, 7, 12, 15]
+puts
+print element_using_each([1, 1, 1, 1, 1, 1]) # => [0, 1, 2, 3, 4, 5]
