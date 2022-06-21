1035bfc706fcbdfd07dab050dfc159cbb8b0d47c
Cosmel Villalobos <cosmelvg@gmail.com>
2022-06-20 23:30:03 -0400

    Update EnumerablesRanges
    - Add format name exercise

---
+++ EnumerablesRanges/format_name.rb
@@ -0,0 +1,23 @@
+# Hint: use str.upcase and str.downcase
+# "abc".upcase # => "ABC"
+
+def format_name(str)
+  str.split(' ').each(&:capitalize!).join(' ')
+end
+
+puts format_name('chase WILSON') # => "Chase Wilson"
+puts format_name('brian CrAwFoRd scoTT') # => "Brian Crawford Scott"
+
+def another_format_name(str)
+  parts = str.split(' ')
+  new_parts = []
+
+  parts.each do |part|
+    new_parts << part[0].upcase + part[1..-1].downcase
+  end
+
+  new_parts.join(' ')
+end
+
+puts another_format_name('chase WILSON') # => "Chase Wilson"
+puts another_format_name('brian CrAwFoRd scoTT') # => "Brian Crawford Scott"
