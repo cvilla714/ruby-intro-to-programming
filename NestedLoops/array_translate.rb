21c73f7528a25185077c3201a94f67fa2e7dea09
Cosmel Villalobos <cosmelvg@gmail.com>
2022-06-26 23:50:36 -0400

    Update Nested Loops folder
    - Added pig_latin
    - Added array_translate

---
+++ NestedLoops/array_translate.rb
@@ -0,0 +1,65 @@
+def array_translate(array)
+  new_word = ''
+  array.each_with_index do |word, idx|
+    current_index = word[idx]
+    # previous_word_index = word[idx - 1]
+    # previous_word =word[previous_word_index]
+    current_number = word
+    next unless current_index.is_a?(Integer)
+
+    # current_number.times { new_word << previous_word }
+
+    # puts "this is the current index: #{current_index}"
+    # puts "this is the previous word index is : #{previous_word_index}"
+    puts "this is the current number: #{current_number}"
+    # puts "this is the previous word: #{previous_word}"
+    # current_index.times { new_word << previous_word }
+  end
+  new_word
+end
+
+print array_translate(['Cat', 2, 'Dog', 3, 'Mouse', 1]); # => "CatCatDogDogDogMouse"
+puts
+
+print array_translate(['red', 3, 'blue', 1]); # => "redredredblue"
+puts
+
+def another_array_translate(array)
+  str = ''
+
+  i = 0
+  while i < array.length
+    ele = array[i]
+    num = array[i + 1]
+    num.times { str += ele }
+
+    i += 2
+  end
+
+  str
+end
+
+puts 'using the while loop'
+print another_array_translate(['Cat', 2, 'Dog', 3, 'Mouse', 1]); # => "CatCatDogDogDogMouse"
+puts
+
+print another_array_translate(['red', 3, 'blue', 1]); # => "redredredblue"
+puts
+
+def new_array_translate(array)
+  new_word = ''
+  array.each_with_index do |word, idx|
+    if word.is_a?(Integer)
+      current_index = idx
+      new_word << array[current_index - 1] * word
+    end
+  end
+  new_word
+end
+
+puts ' the last version'
+print new_array_translate(['Cat', 2, 'Dog', 3, 'Mouse', 1]); # => "CatCatDogDogDogMouse"
+puts
+
+print new_array_translate(['red', 3, 'blue', 1]); # => "redredredblue"
+puts
---
+++ NestedLoops/pig_lating.rb
@@ -0,0 +1,48 @@
+# Pig latin translation uses the following rules:
+# - for words that start with a vowel, add 'yay' to the end
+# - for words that start with a nonvowel, move all letters before the first vowel to the end of the word and add 'ay'
+
+def pig_latin_word(word)
+  if word.match(/^[aeiou]/)
+    word + 'yay'
+  else
+    word[1..-1] + word[0] + 'ay'
+  end
+end
+
+puts pig_latin_word('apple')   # => "appleyay"
+puts pig_latin_word('eat')     # => "eatyay"
+puts pig_latin_word('banana')  # => "ananabay"
+puts pig_latin_word('trash')   # => "ashtray"
+
+def another_pig_latin(word)
+  vowels = %w[a e i o u]
+
+  word + 'yay' if vowels.include?(word[0])
+
+  word.each_char.with_index do |char, idx|
+    return word[idx..-1] + word[0...idx] + 'ay' if vowels.include?(char)
+  end
+end
+
+puts 'this is another version of the pig latin translator'
+puts another_pig_latin('apple')   # => "appleyay"
+puts another_pig_latin('eat')     # => "eatyay"
+puts another_pig_latin('banana')  # => "ananabay"
+puts another_pig_latin('trash')   # => "ashtray"
+
+def new_pig_latin_word(word)
+  vowels = 'aeiou'
+
+  return word + 'yay' if vowels.include?(word[0])
+
+  word.each_char.with_index do |char, i|
+    return word[i..-1] + word[0...i] + 'ay' if vowels.include?(char)
+  end
+end
+
+puts 'this is the new pig latin translator'
+puts new_pig_latin_word('apple')   # => "appleyay"
+puts new_pig_latin_word('eat')     # => "eatyay"
+puts new_pig_latin_word('banana')  # => "ananabay"
+puts new_pig_latin_word('trash')   # => "ashtray"
