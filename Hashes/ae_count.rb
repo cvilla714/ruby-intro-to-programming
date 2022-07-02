f2a9d99d8272a65e1f512e5924158fc2ebf319ab
Cosmel Villalobos <cosmelvg@gmail.com>
2022-07-02 19:08:26 -0400

    Update Hashes folder
    - Include more examples
      - ae_count
      - element_count

--- Hashes/ae_count.rb
+++ Hashes/ae_count.rb
@@ -6,3 +6,12 @@ end
 
 puts ae_count('everyone can program') #=> {"a"=>2, "e"=>3}
 puts ae_count('keyboard') #=> {"a"=>1, "e"=>1}
+
+def another_ae_count(str)
+  count = { 'a' => 0, 'e' => 0 }
+  str.each_char { |char| count[char] += 1 if %w[a e].include?(char) }
+  count
+end
+
+puts another_ae_count('everyone can program') #=> {"a"=>2, "e"=>3}
+puts another_ae_count('keyboard') #=> {"a"=>1, "e"=>1}
