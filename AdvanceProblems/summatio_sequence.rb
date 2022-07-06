d0f33824e7830c1aba7172901e0706fcf19962e3
Cosmel Villalobos <cosmelvg@gmail.com>
2022-07-05 20:11:47 -0400

    Update Advance folder
    - Include new exercises
      - perfect square
      - summation_sequence
      - triple_sequence

---
+++ AdvanceProblems/summatio_sequence.rb
@@ -0,0 +1,30 @@
+def summation(num)
+  (1..num).inject(:+)
+end
+
+puts 'print the summation sequence'
+puts summation(5) # => 15
+puts summation(6) # => 21
+
+def summation_sequence(start, length)
+  seq = [start]
+  length.times { seq << summation(seq[-1]) }
+  seq
+end
+
+puts 'using times method'
+print summation_sequence(3, 4) # => [3, 6, 21, 231]
+puts
+print summation_sequence(5, 3) # => [5, 15, 120]
+puts
+def another_summation_sequence(start, length)
+  seq = [start]
+  seq << summation(seq[-1]) while seq.length < length
+  seq
+end
+
+puts 'using times method'
+print another_summation_sequence(3, 4) # => [3, 6, 21, 231]
+puts
+print another_summation_sequence(5, 3) # => [5, 15, 120]
+puts
---
+++ AdvanceProblems/tripple_sequence.rb
@@ -0,0 +1,33 @@
+def three_times(num)
+  num * 3
+end
+
+puts 'printing the number multiply by 3'
+puts three_times(10) # => 30
+puts three_times(5) # => 15
+puts three_times(3) # => 9
+puts
+
+def triple_sequence(start, length)
+  another = [start]
+  length.times { another << another[-1] * 3 }
+  another
+end
+
+puts 'using times method'
+print triple_sequence(2, 4) # => [2, 6, 18, 54]
+puts
+print triple_sequence(4, 5) # => [4, 12, 36, 108, 324]
+puts
+
+def another_solution(start, length)
+  seq = [start]
+  seq << seq[-1] * 3 while seq.length < length
+  seq
+end
+
+puts 'using while loop'
+print another_solution(2, 4) # => [2, 6, 18, 54]
+puts
+print another_solution(4, 5) # => [4, 12, 36, 108, 324]
+puts
