429944fe18324b1daa5f1c34065b558752462c12
Cosmel Villalobos <cosmelvg@gmail.com>
2022-07-16 18:30:01 -0400

    Update AdvanceProblems
    - collapsible_char
    - consonant_cancel

---
+++ AdvanceProblems/consonant_cancel.rb
@@ -0,0 +1,36 @@
+def replace_letter(word)
+  word.each_char.with_index do |char, i|
+    return word[i..-1] if char.match(/[aeiou]/)
+  end
+end
+
+puts 'testing replace_letter'
+puts replace_letter('hello')
+puts replace_letter('the')
+puts
+
+def consonant_cancel(sentence)
+  sentence.split(' ').map { |word| replace_letter(word) }.join(' ')
+end
+
+puts 'applying solution'
+puts consonant_cancel('down the rabbit hole') #=> "own e abbit ole"
+puts consonant_cancel('writing code is challenging') #=> "iting ode is allenging"
+puts
+
+def another_consonant_cancel(sentence)
+  words = sentence.split
+  new_words = words.map { |word| remove_first_consonant(word) }
+  new_words.join(' ')
+end
+
+def remove_first_consonant(word)
+  vowels = 'aeiou'
+  word.each_char.with_index do |char, i|
+    return word[i..-1] if vowels.include?(char)
+  end
+end
+
+puts 'antoher method'
+puts another_consonant_cancel('down the rabbit hole') #=> "own e abbit ole"
+puts another_consonant_cancel('writing code is challenging') #=> "iting ode is allenging"
