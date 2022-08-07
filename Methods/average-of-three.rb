2bfa0df55272a7b60e4e76545466cee5ce592aca
Cosmel Villalobos <cosmelvg@gmail.com>
2022-08-07 15:42:39 -0400

    Update Methods folder
    - Add debugger to averare_of_three

--- Methods/average-of-three.rb
+++ Methods/average-of-three.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
+require 'byebug'
 def average_of_three(num1, num2, num3)
   sum = num1 + num2 + num3
   average = sum / 3.0
 end
+
+# debugger
+# playing with debugger
+# first_number = average_of_three(1, 2, 3)
+# second_number = first_number + 10
 
 puts average_of_three(3, 7, 8)   # => 6.0
 puts average_of_three(2, 5, 17)  # => 8.0
