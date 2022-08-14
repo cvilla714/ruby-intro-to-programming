1d08d9203f4d881a71b95d31a5fbca6ea4b0c9b7
Cosmel Villalobos <cosmelvg@gmail.com>
2022-08-14 17:37:09 -0400

    Add new exercise
    - Include solution for half_and_doubles
    - Fix path to use relative_require

--- debugging_exercise_1/lib/half_and_double.rb
+++ debugging_exercise_1/lib/half_and_double.rb
@@ -1,19 +1,22 @@
 # Debug this code to pass rspec! There are 1 mistake to fix.
 
-# Write a method, half_and_double, that accepts an array of numbers as an arg. 
+# Write a method, half_and_double, that accepts an array of numbers as an arg.
 # The method should return a new array where every even number is divided by 2 and every odd
 # number is multipled by 2.
 
-require "byebug"
+require 'byebug'
 
+# def half_and_double(array)
+#   array.map do |num|
+# if num.even?
+#   num / 2
+# else
+#   num * 2
+# end
+#   end
+# end
+
+# another way to write it using ternary operator
 def half_and_double(array)
-    new_array = array.map do |num|
-        if num % 2 == 0
-            return num / 2
-        else
-            return num * 2
-        end
-    end
-
-    new_array
+  array.map { |num| num.even? ? num / 2 : num * 2 }
 end
