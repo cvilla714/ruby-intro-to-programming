47560d3826eba81c4bbe1c9d092d82495d388308
Cosmel Villalobos <cosmelvg@gmail.com>
2022-08-28 18:40:41 -0400

    Add new project
    - Include dog project and solution

---
+++ dog_project/lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  def initialize(name, breed, age, bark, favorite_foods)
+    @name = name
+    @breed = breed
+    @age = age
+    @bark = bark
+    @favorite_foods = favorite_foods
+  end
+
+  attr_accessor :age
+  attr_reader :name, :breed, :favorite_foods
+
+  def bark
+    @age > 3 ? @bark.upcase : @bark.downcase
+  end
+
+  def favorite_food?(food)
+    # @favorite_foods.include?(food)
+    @favorite_foods.map(&:downcase).include?(food.downcase)
+  end
+end
--- procs_project/lib/part_2.rb
+++ procs_project/lib/part_2.rb
@@ -1,3 +1,31 @@
 def reverser(string, &proc)
-  proc.call(string)
+  proc.call(string.reverse)
+end
+
+def word_changer(string, &proc)
+  string.split.map { |word| proc.call(word) }.join(' ')
+end
+
+def greater_proc_value(number, proc_1, proc_2)
+  [proc_1.call(number), proc_2.call(number)].max
+end
+
+def and_selector(array, proc_1, proc_2)
+  array.select { |n| proc_1.call(n) && proc_2.call(n) }
+end
+
+# def alternating_mapper(array, proc_1, proc_2)
+# array.each_with_index.map do |n, i|
+# if i.even?
+# proc_1.call(n)
+# else
+# proc_2.call(n)
+# end
+# end
+# end
+
+def alternating_mapper(array, proc_1, proc_2)
+  array.each_with_index.map do |n, i|
+    i.even? ? proc_1.call(n) : proc_2.call(n)
+  end
 end
